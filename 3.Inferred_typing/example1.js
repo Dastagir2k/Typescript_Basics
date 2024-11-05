3b63ee70cbab077e2bdbff5b34e65e83e4c57f89
unknown <dastagir2k@gmail.com>
2024-11-05 22:29:07 +0530

    3.Inferred_typing added

---
+++ 3.Inferred_typing/example1.js
@@ -0,0 +1,4 @@
+// Inferred Typing is which if we did not specify any datatype to variable
+// typescripyt will automatically denotes is data type and have TS functionality and rules.
+var myname = "String";
+console.log(myname);
---
+++ 3.Inferred_typing/example1.ts
@@ -0,0 +1,5 @@
+// Inferred Typing is which if we did not specify any datatype to variable
+// typescripyt will automatically denotes is data type and have TS functionality and rules.
+
+var myname="String";
+console.log(myname);
